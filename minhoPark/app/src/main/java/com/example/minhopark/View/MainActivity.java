1b250b4d5132b86ceec031baf7286e5b3fe97184
Rui Filipe Coelho Moreira <61985785+RuiMoreiraA93232@users.noreply.github.com>
2022-01-23 20:24:04 +0000

    preferencias completas

--- minhoPark/app/src/main/java/com/example/minhopark/View/MainActivity.java
+++ minhoPark/app/src/main/java/com/example/minhopark/View/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.minhopark.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.MinhoPark.R;
 
@@ -15,6 +21,7 @@ public class  MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -26,6 +33,7 @@ public class  MainActivity extends AppCompatActivity {
         buttonPref.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                buscarInformacoesGPS(view);
                 Intent intent = new Intent(MainActivity.this, PreferencesActivity.class);
                 startActivity(intent);
             }
@@ -38,5 +46,32 @@ public class  MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    public void buscarInformacoesGPS(View v) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+            return;
+        }
+
+        LocationManager mlocManager = (LocationManager) getSystemService(MainActivity.this.LOCATION_SERVICE);
+        LocationListener mlocListener = new MinhaLocalizacaoListener();
+
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+
+        if (mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            String texto = "Latitude: " + MinhaLocalizacaoListener.latitude + "\n" +
+                    "Longitude: " + MinhaLocalizacaoListener.longitude + "\n";
+
+            //Toast.makeText(MainActivity.this, texto, Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
